1cfd6524f5378b07b4f8b507e0021d77126a64f5
Kacper <kacperodziemczyk@gmail.com>
2023-06-14 22:22:26 +0200

    React-Router-DOM

--- src/App.js
+++ src/App.js
@@ -6,12 +6,23 @@ import AddEmployee from './components/AddEmployee';
 import EditEmplooyee from './components/EditEmployee';
 import Header from'./components/Header';
 import Employees from './pages/Employees';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Customers from './pages/Customers';
 
 function App() {
   return (
-	<Header>
-		<Employees/>
-	</Header>
+	<BrowserRouter>
+		<Header>
+		
+			<Routes>
+				<Route path='/employees' element={<Employees/>}/>
+				<Route path='/' element='Default Page'/>
+				<Route path='/customers' element={<Customers/>}/>
+			</Routes>
+	
+		{/* <Employees/> */}
+		</Header> 
+	</BrowserRouter>
   );
 }
 

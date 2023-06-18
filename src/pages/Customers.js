a872b0b8d315cd5c6b764b7f6d3a5481a12210e5
Kacper <kacperodziemczyk@gmail.com>
2023-06-18 15:22:42 +0200

    Api Django Foreach Customer Make Info About

--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,4 +1,5 @@
 import { useEffect,useState } from "react"
+import { Link } from "react-router-dom";
 
 export default function Customers(){
 	const [customers,SetCustomers]=useState();
@@ -13,7 +14,7 @@ export default function Customers(){
 	return (
 		<>
 		{customers ? customers.map((customer)=>{
-			return <p>{customer.name}</p>
+			return <p><Link to={`/customers/${customer.id}`} >{customer.name}</Link></p>
 		}): null }
 		</>
 	)

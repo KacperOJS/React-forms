0860f9a6e6c07ddbb8b56794a8984e21c5349ceb
Kacper <kacperodziemczyk@gmail.com>
2023-06-18 11:48:25 +0200

    Connection to API Django

--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,3 +1,20 @@
+import { useEffect,useState } from "react"
+
 export default function Customers(){
-	return <h1>Customers Page</h1>
+	const [customers,SetCustomers]=useState();
+	useEffect(()=>{
+		fetch('http://localhost:8000/api/customers/')
+		.then(res =>res.json())
+		.then(data => {
+			console.log(data)
+			SetCustomers(data.customers)
+		})
+	},[]);
+	return (
+		<>
+		{customers ? customers.map((customer)=>{
+			return <p>{customer.name}</p>
+		}): null }
+		</>
+	)
 }

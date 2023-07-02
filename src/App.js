f29881b5584e33c06f628c948c682772ae89fd81
Kacper <kacperodziemczyk@gmail.com>
2023-07-02 13:58:36 +0200

    JWT token refresh after 3 minutes

--- src/App.js
+++ src/App.js
@@ -10,10 +10,37 @@ import Definition from './components/Definition';
 import Customer from './pages/Customer';
 import NotFound from './pages/NotFound';
 import Login from './pages/Login';
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 export const LoginContext = createContext();
 
 function App() {
+	useEffect(()=>{
+		function refreshTokens(){
+			if(localStorage.refresh){
+				const url ="http://localhost:8000/api/token/refresh/";
+				fetch(url,{
+					method:'POST',
+					headers:{
+						'Content-Type': 'application/json'
+					},
+					body:JSON.stringify({
+						refresh:localStorage.refresh,
+					}),
+				}).then((res)=>{
+					return res.json()
+				}).then((data)=>{
+					console.log(data);
+					localStorage.access = data.access;
+					localStorage.refresh = data.refresh;
+					setLoggedIn(true)
+					
+				})
+			}
+		}
+		const minutes = 1000 * 60;
+		refreshTokens(); 
+		setInterval(refreshTokens, minutes * 3);
+	},[])
 	const [loggedIn ,setLoggedIn]=useState(localStorage.access ? true : false);
 	// const [loggedIn ,setLoggedIn]=useState(false);
   return (

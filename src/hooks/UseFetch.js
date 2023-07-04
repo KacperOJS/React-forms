85c169cefbe2cb32938c2e8faf09f6925f7355ca
Kacper <kacperodziemczyk@gmail.com>
2023-07-04 14:40:23 +0200

    reformat usefetch

--- src/hooks/UseFetch.js
+++ src/hooks/UseFetch.js
@@ -1,33 +1,73 @@
-import { useEffect, useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
 
-export default function useFetch(url,{method,headers,body}) {
-  const [data, setData] = useState(null);
-  const [errorStatus ,setErrorStatus]=useState();
-  const navigate = useNavigate();
-  const location = useLocation();
-  useEffect(() => {
-    fetch(url,{
-		method:method,
-		headers: headers,
-		body: body,
-	})
-      .then((res) => {
-		if(res.status ===401){
-			navigate('/login',{state:{previousUrl:location.pathname}})
-		}
-        if (!res.ok) {
-          throw(res.status);
-        }
-        return res.json();
-      })
-      .then((data) => {
-        setData(data);
-      })
-      .catch((error) => {
-        setErrorStatus(error);
-      });
-  }, [url,headers,body,method,navigate,location]);
+export default function useFetch(url, { method, headers, body } = {}) {
+    const [data, setData] = useState();
+    const [errorStatus, setErrorStatus] = useState();
 
-	return {data,errorStatus};
+    const navigate = useNavigate();
+    const location = useLocation();
+    function request() {
+        fetch(url, {
+            method: method,
+            headers: headers,
+            body: body,
+        })
+            .then((response) => {
+                if (response.status === 401) {
+                    navigate('/login', {
+                        state: {
+                            previousUrl: location.pathname,
+                        },
+                    });
+                }
+                if (!response.ok) {
+                    throw response.status;
+                }
+                return response.json();
+            })
+            .then((data) => {
+                setData(data);
+            })
+            .catch((e) => {
+                setErrorStatus(e);
+            });
+    }
+
+    function appendData(newData) {
+        fetch(url, {
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify(newData),
+        })
+            .then((response) => {
+                if (response.status === 401) {
+                    navigate('/login', {
+                        state: {
+                            previousUrl: location.pathname,
+                        },
+                    });
+                }
+
+                if (!response.ok) {
+                    throw response.status;
+                }
+
+                return response.json();
+            })
+            .then((d) => {
+                const submitted = Object.values(d)[0];
+
+                const newState = { ...data };
+                Object.values(newState)[0].push(submitted);
+
+                setData(newState); //new object, it's seen as a state change
+            })
+            .catch((e) => {
+                console.log(e);
+                setErrorStatus(e);
+            });
+    }
+
+    return { request, appendData, data, errorStatus };
 }

399f10735f253d28aea5f0a54d2dea1a0a8aa84d
Kacper <kacperodziemczyk@gmail.com>
2023-07-04 14:40:31 +0200

    reformat definition

--- src/components/Definition.js
+++ src/components/Definition.js
@@ -1,56 +1,61 @@
-import React, { useEffect } from "react";
-import { Link, useParams } from "react-router-dom";
-import useFetch from "../hooks/UseFetch";
-import NotFound from "../pages/NotFound";
+import { useState, useEffect } from 'react';
+import { useParams, useNavigate, Link, useLocation } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
+
+import useFetch from '../hooks/UseFetch';
 
 export default function Definition() {
-//   const [error, setError] = useState(false);
-//   const [word2, setWord] = useState(null);
-const { search } = useParams();
- const [word,errorStatus] = useFetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${search}`)
- useEffect(()=>{
-	console.log('word' ,word,'status',errorStatus);
- })
-if (errorStatus===404) {
-    return (
-      <>
-        <NotFound/>
-        <Link to="/dictionary">Try Again</Link>
-	</>
-    );
-  }
-  if (errorStatus) {
-    return (
-      <>
-        <div>We can't find information about {search}</div>
-        <Link to="/dictionary">Try Again</Link>
-      </>
-    );
-  }
+    //const [word, setWord] = useState();
+    //const [notFound, setNotFound] = useState(false);
+    //const [error, setError] = useState(false);
+    let { search } = useParams();
 
-  return (
-    <>
-      {word?.[0]?.meannigs ? (
+    const location = useLocation();
+    const navigate = useNavigate();
+    const {
+        request,
+        data: [{ meanings: word }] = [{}],
+        errorStatus,
+    } = useFetch('https://api.dictionaryapi.dev/api/v2/entries/en/' + search);
+
+    useEffect(() => {
+        request();
+    }, [search]);
+
+    if (errorStatus === 404) {
+        return (
+            <>
+				Something went wrong
+                <Link to="/dictionary">Search another</Link>
+            </>
+        );
+    }
+
+    if (errorStatus) {
+        return (
+            <>
+                <p>There was a problem with the server, try again later.</p>
+                <Link to="/dictionary">Search another</Link>
+            </>
+        );
+    }
+
+    return (
         <>
-          <h1>Here is a definition:</h1>
-          {word.map((meaning, idx) => (
-            <p key={idx}>
-              {meaning.partOfSpeech + ": "}
-              {meaning.definitions[0].definition}
-            </p>
-          ))}
-          <p>Search again:</p>
-          <Dictionary />
+            {word ? (
+                <>
+                    <h1>Here is a definition:</h1>
+                    {word.map((meaning) => {
+                        return (
+                            <p key={uuidv4()}>
+                                {meaning.partOfSpeech + ': '}
+                                {meaning.definitions[0].definition}
+                            </p>
+                        );
+                    })}
+                    <p>Search again:</p>
+                </>
+            ) : null}
         </>
-      ) : null}
-    </>
-  );
-}
-
-function Dictionary() {
-  return (
-    <div>
-      {/* Define your dictionary component here */}
-    </div>
-  );
+    );
 }
